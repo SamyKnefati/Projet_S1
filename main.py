7a8d5adcd6fbeeaca0bac5879d93ac3485e614ab
Knefati Samy <samy.knefati@edu.ece.fr>
2020-10-23 10:12:06 +0200

    ajout premier programme

---
+++ main.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as anim
+
+writer = anim.FFMpegWriter(fps=30, codec='libx264')
+
+fig = plt.figure()
+l, = plt.plot([], [], 'k-o')
+
+plt.xlim(-5, 5)
+plt.ylim(-5, 5)
+
+x0, y0 = 0, 0
+
+with writer.saving(fig, "writer_test.mp4", 100):
+    for i in range(100):
+        x0 += 0.1 * np.random.randn()
+        y0 += 0.1 * np.random.randn()
+        l.set_data(x0, y0)
+        writer.grab_frame()
